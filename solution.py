c508a0239951eb3014104e6d65f2e8c1a6fd1159
Andrea Waltlova <andywaltlova@gmail.com>
2021-10-29 00:25:41 +0200

    Refactor code structure

--- solution.py
+++ solution.py
@@ -5,9 +5,12 @@ from trip_search.helpers import load_data, init_parser
 if __name__ == '__main__':
     parser = init_parser()
     args = parser.parse_args()
-    # print(args)
+    print(args)
 
     flights = load_data(args.data)
-    engine = SearchEngine(flights, **vars(args))
-    engine.search()
-    print(engine.get_output())
+    print(flights)
+    # engine = SearchEngine(flights, **vars(args))
+
+    # engine.construct_routes(args.origin)
+    # engine.search(args.origin, args.destination)
+    # print(engine.get_output())
--- trip_search/core.py
+++ trip_search/core.py
@@ -56,23 +56,41 @@ class Airport:
 
 
 class SearchEngine:
-    def __init__(self, flights: list[Flight]) -> None:
+    def __init__(self, flights: list[Flight], restrictions) -> None:
         self.data = flights
-        self.last_output = ''
+        # bags, return_requested, max_trip_price, max_bag_price, passengers, max_stops, exclude
+        self.restrictions = restrictions
+        self.visited = []
+        self._tmp_path = []
+        self.all_paths = []
+        self.output = ''
 
     # Need to somehow construct graph of possible routes
     # nodes - airports, edges - flights
-    def _construct_routes(self, origin):
+    def construct_routes(self, origin):
         pass
         # set graph to origin Airport
 
+        # reset visited and paths
+        self._reset_attributes()
+
+    def _reset_attributes(self):
+        self.visited = []
+        self._tmp_path = []
+        self.all_paths = []
+
     # TODO find all possible routes from origin
     # (result sequences of flights -> Trips)
-    def search(self, origin, destination, bags, return_requested,
-               max_trip_price, max_bag_price, passengers, max_stops,
-               exclude) -> list[Trip]:
-        self._construct_routes(origin)
+    # recursive/iterative DFS
+    def search(self, origin, destination) -> list[Trip]:
+        if self.output:
+            print(
+                'Before next search you have to construct new routes (use `construct_routes(origin)`).')
+            print('If you want to get last output use `get_output()`')
+            return
+
+        # Actual search
         pass
 
     def get_output(self) -> str:
-        return self.last_output
+        return self.output
